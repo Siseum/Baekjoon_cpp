08aa95f4a49a37a34ba7bacd7ce5f8d754acce94
Siseum <62826105+Siseum@users.noreply.github.com>
2021-03-30 20:16:48 +0900

    Create 11726.cpp

---
+++ 11726.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+long long DP[1001];
+
+int main() 
+{
+	cin >> N;
+
+	DP[1] = 1;
+	DP[2] = 2;
+
+	for (int i = 3; i <= N; i++)
+	{
+		DP[i] = (DP[i - 1] + DP[i - 2])%10007;
+	}
+
+	cout << DP[N];
+}
