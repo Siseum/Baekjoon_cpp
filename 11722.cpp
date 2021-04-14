95e071c3d91800b63a03222544dccfdc50b85a02
Siseum <62826105+Siseum@users.noreply.github.com>
2021-04-14 21:02:42 +0900

    Create 11722.cpp

---
+++ 11722.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[1001];
+int dp[1001];
+int Result;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = i - 1; j >= 1; j--)
+		{
+			if (arr[i] < arr[j])
+				dp[i] = max(dp[i], dp[j]);
+		}
+		dp[i] += 1;
+		Result = max(Result, dp[i]);
+	}
+
+	cout << Result;
+}
